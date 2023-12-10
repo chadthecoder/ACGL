3d1777936b04c1d7f78912670c185f881087de13
chadthecoder <mcadamschad@outlook.com>
2023-12-10 12:18:57 -0800

    glfw test 1

---
+++ include/ACGL/GLFW.hpp
@@ -0,0 +1,32 @@
+#ifndef GLFW_CLASS_H
+#define GLFW_CLASS_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+class GLFW
+{
+public:
+	GLFWwindow* window;
+
+    const unsigned int width, height;
+
+    const char *title;
+    // ID reference of Elements Buffer Object
+	//GLuint ID;
+	// Constructor that generates a Elements Buffer Object and links it to indices
+	GLFW(const char *title, unsigned int width, unsigned int height);
+
+    GLFWwindow* getWindow();
+    
+	// Binds the EBO
+	//void Bind();
+	// Unbinds the EBO
+	//void Unbind();
+	// Deletes the EBO
+	//void Delete();
+};
+
+#endif
---
+++ src/GLFW.cpp
@@ -0,0 +1,34 @@
+#include "ACGL/GLFW.hpp"
+
+// Constructor that generates a Elements Buffer Object and links it to indices
+GLFW::GLFW(const char *title, unsigned int width, unsigned int height) : title(title), width(width), height(height)
+{
+	// Initialize GLFW
+	glfwInit();
+
+	// Tell GLFW what version of OpenGL we are using 
+	// In this case we are using OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Tell GLFW we are using the CORE profile
+	// So that means we only have the modern functions
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
+	GLFWwindow* window = glfwCreateWindow(width, height, title, NULL, NULL);
+	// Error check if the window fails to create
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		//return -1;
+	}
+	// Introduce the window into the current context
+	glfwMakeContextCurrent(window);
+}
+
+// Binds the EBO
+GLFWwindow* GLFW::getWindow()
+{
+	return window;
+}
